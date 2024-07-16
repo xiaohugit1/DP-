9643e1205ebfceecf37b3ee9577643df7bab4a4e
unknown <hbw1145142023@163.com>
2024-07-16 22:23:21 +0800

    还有1个bug版

--- 2024.7.16找零问题.cpp
+++ 2024.7.16找零问题.cpp
@@ -1,24 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
-int memo[10000];
-long long cnt=0;
-int mec(long long change,long long memo[]){
+int memo[1000000];
+int cnt=10000;
+int mec(int change,int memo[]){
 	if(change==0)return 0;
 	if (change<0)return -1;
 	if(memo[change]==0){
 		if(change>=5){
-			cnt=min(count,1+mec(change-5,memo));
+			cnt=min(cnt,1+mec(change-5,memo));
 		} 
 		if(change>=2) {
-			cnt=min(count,1+mec(change-2,memo));
+			cnt=min(cnt,1+mec(change-2,memo));
 		}
 		if(change>=1){ 
-			cnt=min(count,1+mec(change-1,memo));
+			cnt=min(cnt,1+mec(change-1,memo));
 		}
 		memo[change]=cnt;
 	}
 	return memo[change];
 }
 int main(){
-	cout<< mec(100000,memo);
+	cout<<mec(7,memo);
 }
