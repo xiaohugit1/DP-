27baf60fdc15cf60094ae1a5ac8e03b580a73cb1
unknown <hbw1145142023@163.com>
2024-07-18 21:08:34 +0800

    完成

--- 硬币问题递推方式__多面额(已写完).cpp
+++ 硬币问题递推方式__多面额(已写完).cpp
@@ -2,8 +2,8 @@
 using namespace std;
 const int MAN=20;
 int memo[MAN];
-int num=3;
-int coins[10]={1,2,5};
+int num=0;
+int coins[10]={0};
 void mec(int change,int memo[]){
 	memo[0]=0;
 	for(int i=1;i<MAN;i++){
@@ -12,8 +12,21 @@ void mec(int change,int memo[]){
 	for(int i=0;i<change;i++){
 		for(int j=0;j<num;j++){
 			if(i>=coins[j]){
-				int tmp
+				int tmp=memo[i-coins[j]];
+				if(memo[i]==-1 or tmp<memo[i]){
+					memo[i]=tmp;
+				}
 			}
 		}
 	}
+}
+int main(){
+	int x=0;
+	cin>>x;
+	while (1){
+		cin>>memo[num++];
+		if(getchar()=='/n') break;
+	}
+	mec(x,memo);
+	return 0;
 }
